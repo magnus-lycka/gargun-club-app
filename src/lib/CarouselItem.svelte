b407860619ea58fe6d1cf8cf130568d7791b17c7
magnuslycka <magnus@thinkware.se>
2021-12-16 16:56:02 +0100

    Initial

---
+++ src/lib/CarouselItem.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let name = 'Quilla-al-Lemra';
+	export let path = 'pics/litchton/1/Quilla-al-Lemra.jpeg';
+</script>
+
+<div data-tip={name} class="carousel-item tooltip tooltip-open tooltip-bottom mb-8">
+	<img height="200" width="200" src={path} alt="picture of {name}" />
+</div>
